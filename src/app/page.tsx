d67d48469ad2634060031e07de1a476d4a3114f9
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2024-03-19 15:48:58 +0600

    fix file issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>Home </div>
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+
+      </div>
+      Home 
+      </div>
   );
 }
