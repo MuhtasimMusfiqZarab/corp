968ae295638f24080d97636759f38c1c8b3813ed
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2024-03-19 15:51:24 +0600

    adding files to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+        {children}
+        </body>
     </html>
   );
 }
